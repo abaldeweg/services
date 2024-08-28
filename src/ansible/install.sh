0b088f19ffa8e349135f6398db1a082a01661479
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-08-28 19:56:16 +0000

    Install package

--- src/ansible/install.sh
+++ src/ansible/install.sh
@@ -3,4 +3,5 @@ set -e
 
 echo "Activating feature 'ansible'"
 
-sudo apt update && sudo apt install -y pipx && sudo pipx install --system-site-packages --include-deps ansible
+sudo apt update
+sudo apt install ansible
