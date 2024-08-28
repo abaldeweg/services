9bf936f9a1575f524ab6d96f698ffb66b9edaf0e
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-08-28 13:38:18 +0000

    Fix path

--- src/ansible/install.sh
+++ src/ansible/install.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Activating feature 'ansible'"
 
-sudo apt update && sudo apt install -y pipx && pipx install --include-deps ansible
+sudo apt update && sudo apt install -y pipx && pipx install --include-deps ansible && pipx ensurepath
