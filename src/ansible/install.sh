b8146092a11cd16f56d4f2c9a9fe364e7e63e81b
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-08-28 19:18:16 +0000

    deps

--- src/ansible/install.sh
+++ src/ansible/install.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Activating feature 'ansible'"
 
-sudo apt update && sudo apt install -y pipx && pipx install --include-deps ansible && pipx ensurepath
+sudo apt update && sudo apt install -y pipx && pipx install --system-site-packages --include-deps ansible && pipx ensurepath
