e4dfb76e96bbf4ade2deb63c253d670806868b70
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-09-20 21:03:56 +0000

    Add missing profile actions to the registry

--- apps/create/src/profiles/registry.ts
+++ apps/create/src/profiles/registry.ts
@@ -1,6 +1,10 @@
 import type { Profile } from './../types/types.js';
+import { appVueAction } from './actions/app_vue.js';
 import { baseAction } from './actions/base.js';
+import { ciAction } from './actions/ci.js';
 import { devcontainerAction } from './actions/devcontainer.js';
+import { packageLibTsAction } from './actions/package_lib_ts.js';
+import { appModuleGoAction } from './actions/app_module_go.js';
 
 /**
  * Registry of available profiles
@@ -8,6 +12,10 @@ import { devcontainerAction } from './actions/devcontainer.js';
 export const profiles: Record<string, Profile> = {
   base: baseAction,
   devcontainer: devcontainerAction,
+  ci: ciAction,
+  appVueAction: appVueAction,
+  packageLibTs: packageLibTsAction,
+  appModuleGo: appModuleGoAction,
 };
 
 /**
