e3b0c4936823d14f0d9f12b1c6200e24919e15be
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-09-20 20:33:13 +0000

    Add base profile

--- apps/create/src/profiles/actions/base.ts
+++ apps/create/src/profiles/actions/base.ts
@@ -1,4 +1,5 @@
-import { createDirs } from '../../helpers/index.js';
+import { log, text } from '@clack/prompts';
+import { copyTemplate, createDirs, createFiles } from '../../helpers/index.js';
 import type { Profile } from '../../types/types.js';
 
 /**
@@ -6,8 +7,43 @@ import type { Profile } from '../../types/types.js';
  */
 export const baseAction: Profile = {
   name: 'base',
-  description: 'Create basic directory structure with apps/, packages/ and scripts/directories',
+  description: 'Create basic directory structure with apps/, packages/ and scripts/ directories',
+  ask: async () => {
+    const projectName = await text({
+      message: 'Whats the name of the project?',
+      placeholder: 'Name',
+      initialValue: 'My Project',
+      validate(value) {
+        if (value.length === 0) return `Value is required!`;
+      },
+    });
+
+    const description = await text({
+      message: 'Whats the description of the project?',
+      placeholder: 'Description',
+      initialValue: 'Description',
+    });
+
+    const license = await text({
+      message: 'Which license did you choose?',
+      placeholder: 'License',
+      initialValue: '',
+    });
+
+
+    return { projectName, description, license };
+  },
   run: async (options) => {
     createDirs(['apps', 'packages', 'scripts']);
+    createFiles([{ path: 'README.md', content: `# ${options.projectName}` }]);
+    copyTemplate({ templateName: 'renovate.json', targetPath: 'renovate.json' });
+    createFiles([{ path: 'LICENSE', content: '' }]);
+    log.info('Created LICENSE file, please update it with the correct license text. https://opensource.org/licenses');
+    copyTemplate({ templateName: 'package.json', targetPath: 'package.json', variables: { description: options.description, license: options.license } });
+    copyTemplate({ templateName: '.gitignore', targetPath: '.gitignore' });
+    copyTemplate({ templateName: '.editorconfig', targetPath: '.editorconfig' });
+    createFiles([{ path: 'pnpm-workspace.yaml', content: 'packages:\n' }]);
+    createFiles([{ path: 'go.work', content: 'go 1.24\n\n' }]);
+    copyTemplate({ templateName: 'Makefile', targetPath: 'Makefile' });
   }
 };
