f884cf35750dd70ee8f9d2898ae6c8bcc18012dd
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2026-04-15 09:30:43 +0000

    refactor(BCode): simplify template structure and remove unused styles

--- apps/@baldeweg/ui/src/components/BCode/BCode.vue
+++ apps/@baldeweg/ui/src/components/BCode/BCode.vue
@@ -1,15 +1,6 @@
 <template>
   <pre
-    class="code u:rounded-xl u:p-xl u:overflow-auto"
-  ><code class="code_body"><slot/></code></pre>
+    class="u:rounded-xl u:p-xl u:overflow-auto u:bg-neutral-200"
+  ><code class="u:font-mono"><slot/></code>
+  </pre>
 </template>
-
-<style>
-.code {
-  background: var(--color-neutral-02);
-}
-
-.code_body {
-  font-family: var(--font-monospace);
-}
-</style>
